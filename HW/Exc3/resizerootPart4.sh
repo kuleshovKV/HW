7a8f63fa606bbefc1fb49c14deea7a3b6cfefd11
kuleshovKV <124785884+kuleshovKV@users.noreply.github.com>
2024-06-24 00:59:32 +0300

    Delete HW/Exc3/resizerootPart4.sh

--- HW/Exc3/resizerootPart4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grub2-mkconfig -o /boot/grub2/grub.cfg
-cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g;  s/.img//g"` --force; done
-sed -i "s|rd.lvm.lv=vg_root/lv_root|rd.lvm.lv=VolGroup00/LogVol00|g" /boot/grub2/grub.cfg
